99095c757dec5e6d21fd86b8d8fcf24b6dac67a9
Zhu Li <zhuli19901106@gmail.com>
2019-10-06 10:51:39 +0800

    add responsive demo page

---
+++ coursera/rwd/js/main.js
@@ -0,0 +1,5 @@
+function showResolution() {
+    var body = document.body;
+    var div = document.getElementById('resolution_div');
+    div.innerHTML = window.innerWidth + " * " + window.innerHeight;
+}
